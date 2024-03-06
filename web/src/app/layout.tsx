50c602bf30ca533975c8104afabc091bc3bc07f0
Cristiano Souza <cristiano.souza@windriver.com>
2024-03-06 11:20:32 -0300

    header and main ready

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,11 +1,16 @@
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
+import type { Metadata } from "next";
+import { Barlow_Semi_Condensed } from "next/font/google";
+import "./theme-config.css";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const barlow = Barlow_Semi_Condensed({
+  weight: "700",
+  subsets: ["latin"],
+  variable: "--font-barlow"
+});
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Rock, Paper, Scissors",
@@ -18,14 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="">
-        <Theme appearance="dark" accentColor="crimson" grayColor="sand" radius="large">
+    <html lang="en" className={`${barlow.variable}`}>
+      <body>
+        <Theme radius="large">
           {children}
-          <div className="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-            Coded by <a href="#">Your Name Here</a>.
-          </div>
         </Theme>
       </body>
     </html>
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,32 +1,53 @@
-import { Box, Card, Container, Grid } from "@radix-ui/themes";
+import { Card, Flex, Grid, IconButton } from "@radix-ui/themes";
+import PaperIcon from "../../images/icon-paper.svg";
+import RockIcon from "../../images/icon-rock.svg";
+import ScissorsIcon from "../../images/icon-scissors.svg";
 
+import Image from "next/image";
 export default function Home() {
   return (
-    <Grid columns="1" width="auto">
-      <Box height="9" className="header mx-10 my-2">
-        <Card size="1">
-          <div className="title">
-            <h1>Rock<br />Paper<br />Scissors</h1>
-          </div>
-          <div className="score">Score `${12}`</div>
-        </Card>
-      </Box>
-      <Box height="9">
-        <main>
-          <div className="handOptions">
-            <div className="option">rock</div>
-            <div className="option">paper</div>
-            <div className="option">scissor</div>
-          </div>
-        </main>
-      </Box>
-      <Box height="9">
-        <footer>
-          <div className="rules">
-            <button>Rules</button>
-          </div>
-        </footer>
-      </Box>
-    </Grid>
+    <Grid className="home" height="100vh" align="center" justify="center">
+      <header>
+        <Flex align="center" justify="center">
+          <Card className="header">
+            <Flex align="center" justify="between">
+              <div className="title">
+                <h1>Rock<br />Paper<br />Scissors</h1>
+              </div>
+              <Card className="score">
+                <Flex direction="column" align="center" justify="center" className="px-4">
+                  <p className="scoreTitle">Score</p>
+                  <p className="scoreValue">12</p>
+                </Flex>
+              </Card>
+            </Flex>
+          </Card>
+        </Flex>
+      </header>
+      <main>
+        <Flex align="center" justify="center">
+          <Grid className="handOptions" columns="3" rows="2" height="15rem" width="15rem">
+            <IconButton className="rock">
+              <Image src={RockIcon} alt="Rock" />
+            </IconButton>
+            <IconButton className="paper">
+              <Image src={PaperIcon} alt="Paper" />
+            </IconButton>
+            <IconButton className="scissors">
+              <Image src={ScissorsIcon} alt="Scissors" />
+            </IconButton>
+          </Grid>
+        </Flex>
+      </main>
+      <footer>
+        <div className="rules">
+          <button>Rules</button>
+        </div>
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+          Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </footer>
+    </Grid >
   );
 }
